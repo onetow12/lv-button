0f38e7a124434373784091c4d1ee629c5e016375
uzzo-ba <taras.kolos@metisoft.it>
2022-09-27 14:50:45 +0200

    commit 1

---
+++ src/components/LearnVueButton.vue
@@ -0,0 +1,12 @@
+<template>
+  <button class="lv-button">
+    <slot />
+  </button>
+</template>
+<style scoped>
+.lv-button{
+    border: one;
+    background: red;
+    padding:1rem;
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build:{
+    lib:{
+      entry: resolve(__dirname, "src/index.js"),
+      name:"lv-button",
+      fileName: (format)=> `${format}.js`
+    },
+    rollupOptions:{
+      external:["vue"],
+      output:{
+        globals:{
+          vue:"Vue"
+        }
+      }
+    }
+
+  },
+  plugins: [vue()]
+})

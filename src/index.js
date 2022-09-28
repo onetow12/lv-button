aa10dca30b047aa017385bbf01ac382db2f226dc
uzzo-ba <taras.kolos@metisoft.it>
2022-09-28 10:15:43 +0200

    build fix 2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import LearnVueButton from "./components/LearnVueButton.vue";
 import Input from "./components/Input.vue";
 
-// export default {
-//     install: (app,options)=> {
-//         app.component("LearnVueButton", LearnVueButton)
-//     } 
-// }
-export { LearnVueButton, Input }
+export default {
+    install: (app,options)=> {
+        app.component("LearnVueButton", LearnVueButton).component("Input",Input)
+    } 
+}
--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     lib:{
       entry: resolve(__dirname, "src/index.js"),
       name:"lv-button",
-      fileName: `lv-button`
+      fileName: (format)=> `lv-button.${format}.js` 
     },
     rollupOptions:{
       external:["vue"],
